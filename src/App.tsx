39b9fbe00f004c54503760b7befe2bd5808cdcdb
Madhav Kabra <madhav.kabra1100@gmail.com>
2026-01-28 13:50:48 +0530

    feat: add Recoil state management with event form atoms

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import MainLayout from './layouts/MainLayout';
 import CreateEvent from './pages/CreateEvent';
 
 function App() {
   return (
-    <Router>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<CreateEvent />} />
-        </Routes>
-      </MainLayout>
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<CreateEvent />} />
+          </Routes>
+        </MainLayout>
+      </Router>
+    </RecoilRoot>
   );
 }
 
---
+++ src/store/eventAtoms.ts
@@ -0,0 +1,35 @@
+import { atom } from 'recoil';
+
+export interface EventFormData {
+  // Event details
+  eventName: string;
+  phoneNumber: string;
+  dateTime: string;
+  location: string;
+  costPerPerson: string;
+  description: string;
+  
+  // Images
+  previewImage: string | null;
+  backgroundImage: string | null;
+  
+  // Optional fields
+  capacity?: string;
+  photoGallery?: string[];
+  links?: string[];
+}
+
+export const eventFormState = atom<EventFormData>({
+  key: 'eventFormState',
+  default: {
+    eventName: '',
+    phoneNumber: '',
+    dateTime: '',
+    location: '',
+    costPerPerson: '',
+    description: '',
+    previewImage: null,
+    backgroundImage: null,
+  },
+});
+
