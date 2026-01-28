cd2cf88cf18ba59cbccbbcd16776a369c11695dc
Madhav Kabra <madhav.kabra1100@gmail.com>
2026-01-28 14:53:02 +0530

    style: add custom colors and clean up Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,13 @@ export default {
         'dark-purple': '#1e1b4b',
         'glass-dark': 'rgba(0, 0, 0, 0.2)',
         'glass-light': 'rgba(255, 255, 255, 0.2)',
+        'material-ultrathin-1': '#9C9C9C',
+        'material-ultrathin-2': 'rgba(37, 37, 37, 0.55)',
       },
       backdropBlur: {
         xs: '2px',
+        material: '30px',
+        heavy: '100px',
       },
     },
   },
